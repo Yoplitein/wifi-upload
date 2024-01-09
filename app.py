976a7745253abdacc1ae8275b6ee97a06031833a
Steven Dwy <yoplitein+git@gmail.com>
2024-01-08 17:23:36 -0800

    Allow uploading several files at once

--- app.py
+++ app.py
@@ -15,15 +15,20 @@ def index():
 @app.post("/")
 def upload():
     try:
-        file = request.files["file"]
-        assert file.filename != ""
-        ext = path.splitext(file.filename)[1].strip(".")
-        assert ext != ""
-        name = get_filename(ext)
-        file.save(path.join("uploads", name))
+        files = request.files.getlist("file")
+        files = [f for f in files if f.filename != ""]
+        assert len(files) > 0
+        for i, file in enumerate(files):
+            file = request.files["file"]
+            ext = path.splitext(file.filename)[1].strip(".")
+            assert ext != ""
+            name = get_filename(ext, (i + 1) if len(files) > 1 else None)
+            file.save(path.join("uploads", name))
         return render_template("index.html", success = True)
     except:
         return render_template("index.html", success = False)
 
-def get_filename(ext):
-    return datetime.now().strftime(f"%Y-%m-%d_%H-%M-%S.{ext}")
+def get_filename(ext, seq):
+    if seq != None:
+        seq = f"_{seq}"
+    return datetime.now().strftime(f"%Y-%m-%d_%H-%M-%S{seq}.{ext}")
